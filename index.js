2faf823537a8af7bd394dab5f4733f292d6a9d00
Ravaka82 <RakotonarivoSaholintsoaRavaka@gmail.com>
2023-02-18 18:16:10 +0300

    add script

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express(); // new instance express
 const PORT = 4000;
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello Ravaka 🎉')
   })
